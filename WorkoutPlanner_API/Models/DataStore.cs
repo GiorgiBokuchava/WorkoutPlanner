c52e1b6f83984c97cb759ff37d7e7730031a9f0e
Giorgi <161084613+GiorgiBokuchava@users.noreply.github.com>
2025-05-19 16:35:24 +0400

    Homework 02: add CRUD controllers + Swagger docs (#1)
    * Homework 02: add CRUD controllers + Swagger docs
    
    * Remove minimal‐API stub; keep Controllers + Swagger Program.cs
    
    * Use LINQ; move Swagger to appsettings.json

---
+++ WorkoutPlanner_API/Models/DataStore.cs
@@ -0,0 +1,11 @@
+﻿using WorkoutPlanner_API.Models;
+
+namespace WorkoutPlanner_API.Data;
+
+public static class DataStore
+{
+    public static List<User> Users { get; } = new();
+    public static List<Exercise> Exercises { get; } = new();
+    public static List<Routine> Routines { get; } = new();
+    public static List<WorkoutLog> WorkoutLogs { get; } = new();
+}
--- WorkoutPlanner_API/Program.cs
+++ WorkoutPlanner_API/Program.cs
@@ -1,25 +1,69 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+var swagger = builder.Configuration.GetSection("Swagger");
+
+builder.Services.AddSwaggerGen(c =>
+{
+    var version = swagger["Version"];
+    var title = swagger["Title"];
+    var description = swagger["Description"];
+
+    var contactSection = swagger.GetSection("Contact");
+    var licenseSection = swagger.GetSection("License");
+
+    c.SwaggerDoc(version, new OpenApiInfo
+    {
+        Version = version,
+        Title = title,
+        Description = description,
+        TermsOfService = UriOrNull(swagger["TermsOfService"]),
+        Contact = new OpenApiContact
+        {
+            Name = contactSection["Name"],
+            Url = UriOrNull(contactSection["Url"])
+        },
+        License = new OpenApiLicense
+        {
+            Name = licenseSection["Name"],
+            Url = UriOrNull(licenseSection["Url"])
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath)) c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(ui =>
+    {
+        var version = swagger["Version"];
+        var title = swagger["Title"];
+        ui.SwaggerEndpoint($"/swagger/{version}/swagger.json", $"{title} {version}");
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+static Uri? UriOrNull(string? url)
+{
+    if (string.IsNullOrWhiteSpace(url))
+    {
+        return null;
+    }
+
+    return new Uri(url);
+}
