e540e263d9732a8a58cea28008d4feaf16db4252
GiorgiBokuchava <1giorgibokuchava@gmail.com>
2025-06-02 23:21:14 +0400

    HW-6: further tweaks after review

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
+using WorkoutPlanner.Application.Interfaces.Repositories;
 using WorkoutPlanner.Application.Interfaces.Services;
 using WorkoutPlanner.Application.Services;
 using WorkoutPlanner.Infrastructure.Options;
 using WorkoutPlanner.Infrastructure.Persistance;
 using WorkoutPlanner.Infrastructure.Persistence;
-using WorkoutPlanner.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
