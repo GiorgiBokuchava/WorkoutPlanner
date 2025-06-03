83f4b41a5aa03d58b752d42c6f4580bd2c2e5665
Giorgi <161084613+GiorgiBokuchava@users.noreply.github.com>
2025-06-03 12:54:59 +0400

    Homework/06 advanced .net (#9)
    * Finish HW5: refactor services/repositories per feedback
    
    * Create HW_6_README.md
    
    * HW-6: further tweaks after review
    
    * HW-6: restore user commands/validator, update SqlUserRepository, Program.cs, and add NuGet refs

---
+++ Application/Mappings/WorkoutMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WorkoutPlanner.Contracts;
+using WorkoutPlanner.Domain.Entities;
+
+namespace WorkoutPlanner.Application.Mappings;
+
+public class WorkoutMappingProfile : Profile
+{
+	public WorkoutMappingProfile()
+	{
+		CreateMap<CreateUserRequest, User>();
+		CreateMap<UpdateUserRequest, User>();
+
+		CreateMap<User, UserDto>();
+	}
+}
---
+++ Application/Users/Handlers/CreateUserHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using WorkoutPlanner.Application.Interfaces.Services;
+using WorkoutPlanner.Contracts;
+
+namespace WorkoutPlanner.Application.Users.Handlers;
+
+public sealed class CreateUserHandler
+	: IRequestHandler<CreateUserCommand, UserDto>
+{
+	private readonly IUserService _users;
+
+	public CreateUserHandler(IUserService users) => _users = users;
+
+	public async Task<UserDto> Handle(CreateUserCommand c, CancellationToken ct)
+	{
+		var req = new CreateUserRequest(c.Name, c.Email, c.PasswordHash);
+		return await _users.CreateUserAsync(req);
+	}
+}
